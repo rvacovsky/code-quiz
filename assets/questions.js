67ab2d2430efb792f43b32f1a5abfb8497a68916
Roger Vacovsky <rvacovsky@gmail.com>
2022-03-26 22:31:27 -0400

    initial js, added questions js file

---
+++ assets/questions.js
@@ -0,0 +1,36 @@
+// 1. Once you start the quiz the timer will start at <span>75 seconds</span> and you will be presented with your first question.
+// 2. Once you answer your first question you will go on to the next question.
+// 3. If you answer the question incorrectly, time will be subtracted from the clock. (No pressure!)
+// 4. Once you've answered all of the questions the time that is left will be your score!
+// 5. Have fun! Play again to see if you can beat your high score.
+
+// What year was JavaScript invented?
+// 1998
+// 1995
+// 2005
+// 2011
+
+// What is JavaScript?
+// The bane of my existence
+// Writing a letter using ink extracted from coffee beans
+// The national lanugage of an island in Indonesia
+// A programming lanugage used to create dynamic and interactive web content
+
+// What is the percentage of websites that use JavaScript today?
+// 50%
+// 80%
+// 90%
+// 97%
+
+// If I need to perform a particular task in JavaScript, what block of code do I write?
+// event
+// operator
+// function
+// parameter
+
+// What do I use to create an array?
+// parentheses
+// brackets
+// curly brackets
+// an array gun
+
--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,23 @@
-// 1. Once you start the quiz the timer will start at <span>75 seconds</span> and you will be presented with your first question.
-// 2. Once you answer your first question you will go on to the next question.
-// 3. If you answer the question incorrectly, time will be subtracted from the clock. (No pressure!)
-// 4. Once you've answered all of the questions the time that is left will be your score!
-// 5. Have fun! Play again to see if you can beat your high score.
+// Elements
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
 
-// What year was JavaScript invented?
-// 1998
-// 1995
-// 2005
-// 2011
+//When 'Start Quiz' is clicked, whos the Info Box
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo");
+}
 
-// What is JavaScript?
-// The bane of my existence
-// Writing a letter using ink extracted from coffee beans
-// The national lanugage of an island in Indonesia
-// A programming lanugage used to create dynamic and interactive web content
+//When 'Exit Quiz' is clicked, hides Info Box
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo");
+}
 
-// What is the percentage of websites that use JavaScript today?
-// 50%
-// 80%
-// 90%
-// 97%
+//When 'Continue' is clicked, proceeds to quiz
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo");
+    quiz_box.classList.add("activeQuiz");
+}
 
-// If I need to perform a particular task in JavaScript, what block of code do I write?
-// event
-// operator
-// function
-// parameter
-
-// What do I use to create an array?
-// parentheses
-// brackets
-// curly brackets
-// an array gun
